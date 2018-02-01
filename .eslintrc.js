5130919aa5a67244c1fc29dd5f8fb79b50d25ff4
Brian Whicheloe <>
2018-01-31 22:25:40 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: 'brian',
+    env: {
+        jest: true,
+    },
+}
---
+++ client/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        App Stuff<br>
+        bar: {{bar}}
+    </div>
+</template>
+
+<script>
+const bar = require('./bar.js')
+export default {
+    name: 'app',
+    data() {
+        return {
+            bar: bar,
+        }
+    },
+}
+</script>
